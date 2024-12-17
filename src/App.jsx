41ae516fdd2a0077a717fc060d2302dd8e647c29
Lationcesanjay <91967359+Lationcesanjay@users.noreply.github.com>
2024-12-17 18:24:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Game} from './components/ticTac'
+  export default function App() {
+  return (
+     
+      
+      <div>
+        <Game/>
+       </div>
+    
+  );
+}
